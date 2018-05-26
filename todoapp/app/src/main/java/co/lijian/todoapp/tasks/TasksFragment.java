9c88c75ee748a536aead179ff0d9d320f11ac001
Li Jian <lijian@hantek.com>
2018-05-26 15:59:41 +0800

    红色添加按钮点击后有反应

--- todoapp/app/src/main/java/co/lijian/todoapp/tasks/TasksFragment.java
+++ todoapp/app/src/main/java/co/lijian/todoapp/tasks/TasksFragment.java
@@ -1,18 +1,51 @@
 package co.lijian.todoapp.tasks;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import co.lijian.todoapp.R;
+
+import static com.google.common.base.Preconditions.checkNotNull;
 
 /**
  * Display a grid of {@link Task}s. User can choose to view all, active or completed tasks.
  */
 public class TasksFragment extends Fragment implements TasksContract.View {
 
+    private TasksContract.Presenter mPresenter;
+
     public static TasksFragment newInstance() {
         return new TasksFragment();
     }
 
     @Override
-    public void setPresnter(TasksContract.Presenter presnter) {
+    public void setPresnter(TasksContract.Presenter presenter) {
+        mPresenter = checkNotNull(presenter);
+    }
 
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.tasks_frag, container, false);
+
+        // Set up floating action button
+        FloatingActionButton fab =
+                (FloatingActionButton) getActivity().findViewById(R.id.fab_add_task);
+
+        fab.setImageResource(R.drawable.ic_add);
+        // 调用 setOnClickListener 后，点击 UI　会有反应
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //mPresenter.addNewTask();
+            }
+        });
+
+        return root;
     }
 }
