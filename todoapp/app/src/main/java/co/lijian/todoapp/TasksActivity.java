a599bce1d6a94413ab829caeaac67818c238beb4
Li Jian <lijian@hantek.com>
2018-05-26 08:12:19 +0800

    基本完成 TasksActivity 的 onCreate 方法

--- todoapp/app/src/main/java/co/lijian/todoapp/TasksActivity.java
+++ todoapp/app/src/main/java/co/lijian/todoapp/TasksActivity.java
@@ -1,13 +1,75 @@
 package co.lijian.todoapp;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+import co.lijian.todoapp.util.ActivityUtils;
 
 public class TasksActivity extends AppCompatActivity {
+
+    private DrawerLayout mDrawerLayout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.tasks_act);
+
+        // Set up the toolbar.
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar ab = getSupportActionBar();
+        ab.setHomeAsUpIndicator(R.drawable.ic_menu);
+        ab.setDisplayHomeAsUpEnabled(true);
+
+        // Set up the navigation drawer.
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+        mDrawerLayout.setStatusBarBackground(R.color.colorPrimaryDark);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        if (navigationView != null) {
+            setupDrawerContent(navigationView);
+        }
+
+        TasksFragment tasksFragment =
+                (TasksFragment)getSupportFragmentManager().findFragmentById(R.id.contentFrame);
+        if (tasksFragment == null) {
+            // Create the fragment
+            tasksFragment = TasksFragment.newInstance();
+            ActivityUtils.addFragmentToActivity(
+                    getSupportFragmentManager(), tasksFragment, R.id.contentFrame);
+        }
+    }
+
+    private void setupDrawerContent(NavigationView navigationView)  {
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                        switch (menuItem.getItemId()) {
+                            case R.id.list_navigation_menu_item:
+                                // Do nothing, we're already on that screen
+                                break;
+                            case R.id.statistics_navigation_menu_item:
+                                Intent intent =
+                                        new Intent(TasksActivity.this, StatisticsActivity.class);
+                                startActivity(intent);
+                                break;
+                             default:
+                                break;
+                        }
+                        // Close the navigation drawer when an item is selected.
+                        menuItem.setChecked(true);
+                        mDrawerLayout.closeDrawers();
+                        return true;
+                    }
+                }
+        );
     }
 }
---
+++ todoapp/app/src/main/java/co/lijian/todoapp/TasksFragment.java
@@ -0,0 +1,13 @@
+package co.lijian.todoapp;
+
+import android.support.v4.app.Fragment;
+
+/**
+ * Display a grid of {@link Task}s. User can choose to view all, active or completed tasks.
+ */
+public class TasksFragment extends Fragment {
+
+    public static TasksFragment newInstance() {
+        return new TasksFragment();
+    }
+}
